5953a290031852283662080c0cfc79ea66322d35
radekcz <radek.krizka@gmail.com>
2021-04-06 11:37:39 +0200

    add parameter topic for KafkaProducer

--- src/main/java/cz/rk/kafka/producer/KafkaProducer.java
+++ src/main/java/cz/rk/kafka/producer/KafkaProducer.java
@@ -3,8 +3,12 @@ package cz.rk.kafka.producer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 /**
  * Kafka producer
@@ -13,14 +17,28 @@ import org.springframework.stereotype.Service;
 public class KafkaProducer {
 
     private static final Logger logger = LoggerFactory.getLogger(KafkaProducer.class);
-    private static final String TOPIC = "first_topic";
+    private String topic;
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
 
+    public KafkaProducer(@Value("${kafka.producer.topic}") String topic) {
+        this.topic = topic;
+    }
+
     public void sendMessage(String message) {
         logger.info(String.format("#### -> Producing message -> %s", message));
-        this.kafkaTemplate.send(TOPIC, message);
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic, message);
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+            @Override
+            public void onFailure(Throwable throwable) {
+                logger.error("#### Failure - unable to send message=[" + message + "] due to : " + throwable.getMessage());
+            }
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                logger.info("#### Success - sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+        });
     }
 }
